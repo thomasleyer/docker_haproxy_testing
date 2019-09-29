ad6ca714f5b79b6439862baa9c4c8144e78fbe7f
Thomas Leyer <selecticon@googlemail.com>
2019-09-29 21:42:31 +0200

    Provide tests, create an empty haproxy.conf, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # docker_haproxy_testing
 An example for testing docker images that die instantly in testing context
+
+## Motivation
+
+Invoking docker run on the haproxy image will, without further care, make the container die before tests have been
+conducted:
+
+```
+#docker run -it haproxy:latest
+[ALERT] 271/191340 (1) : Cannot open configuration file/directory /usr/local/etc/haproxy/haproxy.cfg : No such file or directory
+```
+
+## Idea
+
+Spawn an instance with an alternative cmd (sleep 600) without the need to make special adjustments (e.g. defining a cmd
+that would make the container sleep during testing based on environment variables).
+
+Using ruby system()-calls in rspec's before and after steps.
+
+## What it looks like
+
+```
+#rspec spec/
+
+image haproxy:latest
+27e173e4183c3f25e38be88053aaa3c995e498cc67305efba31152fd555ac102
+  Docker Build from id: "haproxy:latest"
+    cmd
+      should eq ["haproxy", "-f", "/usr/local/etc/haproxy/haproxy.cfg"]
+  run image haproxy:latest
+    File "/usr/local/etc/haproxy/haproxy.cfg"
+      should exist
+    File "/usr/local/sbin/haproxy"
+      should exist
+      should be file
+      should be executable
+      should be owned by "root"
+testhaproxy
+
+Finished in 8.29 seconds (files took 0.43959 seconds to load)
+6 examples, 0 failures
+
+#
+```
---
+++ spec/docker_haproxy_spec.rb
@@ -0,0 +1,53 @@
+require 'dockerspec/serverspec'
+
+image = 'haproxy'
+tag = 'latest'
+haproxyconf = "haproxy.conf"
+
+describe "image #{image}:#{tag}"  do
+
+  ## Preparation steps
+  before(:all) do
+    command = ""
+    if (tag)
+      testimage = "#{image}:#{tag}"
+    else
+      testimage = "#{image}"
+    end
+    haproxyconf = ""
+    currentdir = Dir.pwd
+    if (haproxyconf)
+      testhaproxyconf = "-v #{currentdir}/#{haproxyconf}:/usr/local/etc/haproxy/haproxy.cfg"
+    end
+    output = system("docker run --rm -d --name test#{image} #{testhaproxyconf} #{image}:#{tag} sleep 600" )
+    @container = Docker::Container.get("test#{image}")
+      set :backend, :docker
+      set :docker_container, @container.id
+  end
+
+  ## Testing the image
+  describe docker_build(id: "#{image}:#{tag}") do
+    its(:cmd) { should eq ["haproxy", "-f", "/usr/local/etc/haproxy/haproxy.cfg"] }
+  end
+
+  ## Testing the container
+  describe "run image #{image}:#{tag}"  do
+    describe file ('/usr/local/etc/haproxy/haproxy.cfg') do
+      it { should exist }
+    end
+    describe file ('/usr/local/sbin/haproxy') do
+      it { should exist }
+      it { should be_file }
+      it { should be_executable }
+      it { should be_owned_by 'root' }
+    end
+  end
+
+  ## Cleaning up the mass
+  after(:all) do
+    system ("docker rm -f test#{image}")
+  end
+end
+
+
+
